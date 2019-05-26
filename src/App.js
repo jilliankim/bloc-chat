b3bdc4a7b9d86ec5a956d7ef3a26b6cb480014fd
Jillian Kim <jillianrkim12@gmail.com>
2019-05-26 10:26:08 -0700

    stuffs

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component} from 'react';
+import RoomList from './components/RoomList';
+import MessageList from './components/MessageList';
+import './App.css';
+import * as firebase from 'firebase';
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBk0kiiFRbwyPcKKY6CwHvKAV0wOg2M0l4",
+  authDomain: "bloc-chat-7c769.firebaseapp.com",
+  databaseURL: "https://bloc-chat-7c769.firebaseio.com",
+  projectId: "bloc-chat-7c769",
+  storageBucket: "bloc-chat-7c769.appspot.com",
+  messagingSenderId: "596692860351",
+  appId: "1:596692860351:web:b25a663f14189c8a"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {activeRoom: ""};
+    this.activeRoom = this.activeRoom.bind(this);
+  }
+
+  activeRoom(room) {
+    this.setState({ activeRoom: room })
+  }
+
+  render() {
+    const showMessages = this.state.activeRoom;
+    return (
+      <div className="App">
+      <h1>{this.state.activeRoom.title || "Choose a Room"}</h1>
+        <RoomList firebase={firebase} activeRoom={this.activeRoom} />
+        { showMessages ?
+          (<MessageList firebase={firebase} activeRoom={this.state.activeRoom.key}/>)
+          : (null)
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/RoomList.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+export class RoomList extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        name: "",
+        rooms: [],
+    };
+
+  this.roomsRef = this.props.firebase.database().ref('rooms');
+  this.handleChange = this.handleChange.bind(this);
+  this.createRoom = this.createRoom.bind(this);
+
+  }
+
+    handleChange(e){
+      this.setState({ name: e.target.value })
+    }
+
+    componentDidMount() {
+      this.roomsRef.on('child_added', snapshot => {
+        const room = snapshot.val();
+        room.key = snapshot.key;
+        this.setState({ rooms: this.state.rooms.concat( room ) })
+      });
+    }
+
+    selectRoom(room) {
+      this.props.activeRoom(room);
+    }
+
+
+    createRoom(e) {
+    e.preventDefault();
+    this.roomsRef.push({ name: this.state.name });
+    this.setState({name: ""})
+    }
+
+    render(){
+      const roomList = this.state.rooms.map((room) =>
+      <li key={room.key} onClick={(e) => this.selectRoom(room, e)}>{room.name}</li>);
+      const newRoom = (
+      <form onSubmit={this.createRoom}>
+        <input type="text" value={this.state.name} onChange={this.handleChange} placeholder="Create new chat room"  />
+        <input type="submit" value="Start Chat"/>
+      </form>
+    );
+      return(
+        <section>
+          <div>{newRoom}</div>
+          <ul>{roomList}</ul>
+        </section>
+
+      );
+      //second checkpoint create room
+
+
+    }
+}
+export default RoomList;
